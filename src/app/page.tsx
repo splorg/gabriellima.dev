5b9b09641f2323ce83e38c726f6708444d532aaf
Gabriel Lima <gabriel.limabs02@gmail.com>
2025-09-02 11:17:14 -0300

    feat: reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { AboutMe } from "@/components/about-me";
-import { Profile } from "@/components/profile";
-import { Projects } from "@/components/projects";
-import { Work } from "@/components/work";
-
-const Home = () => {
-	return (
-		<main className="mx-auto mb-8 max-w-2xl">
-			<Profile />
-			<div>
-				<AboutMe />
-				<Work />
-				<Projects />
-			</div>
-		</main>
-	);
-};
-
-export default Home;
+export default function Home() {
+  return <main>Hello, world!</main>;
+}
